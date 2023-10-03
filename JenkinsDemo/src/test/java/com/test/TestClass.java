5f61d2fc18d3f7d0e66a22176b3b45b4776b5e81
Your Name <you@example.com>
2023-10-03 23:45:51 +0530

    new code for browser parameter

--- JenkinsDemo/src/test/java/com/test/TestClass.java
+++ JenkinsDemo/src/test/java/com/test/TestClass.java
@@ -1,7 +1,10 @@
 package com.test;
 
+import java.util.concurrent.ForkJoinPool.ManagedBlocker;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
@@ -14,9 +17,17 @@ public class TestClass
 	@BeforeMethod
     public void setup()
     {
-    	 driver = new ChromeDriver();
-    	driver.manage().window().maximize();
-    	driver.get("https://www.google.com/");
+    	 if(System.getProperty("browser").equalsIgnoreCase("Chrome"))
+    	 {
+    		 driver = new ChromeDriver();
+    	 }
+    	 else if (System.getProperty("browser").equalsIgnoreCase("Firefox"))
+    	 {
+    		 driver = new FirefoxDriver();
+		 }
+    	 
+    	 driver.manage().window().maximize();
+    	 driver.get(System.getProperty("url"));
     }
 
     
